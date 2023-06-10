eacfffad9b4fd6856ddec146fe54fbe81b06563d
Sneha Tarnekar <snehatarnekar@gmail.com>
2023-06-10 11:08:43 +0530

    use variables, read arguments, read input from user

---
+++ variable.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#read first argument
+echo "First argument is $1"
+
+#read second argument
+echo "Second argument is $2"
+
+#read all arguments
+echo "$@ are all arguments"
+
+#use variable
+name="Jessica"
+echo "Hello $name"
+
+#read input from user
+echo "Enter the name"
+read username
+
+echo "The name is $username"
+
